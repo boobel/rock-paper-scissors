f71df7c53c78c66cb73f15a489495ac49df132c4
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-05-18 23:50:51 +0200

    finished basic functionality, added score counter

--- script.js
+++ script.js
@@ -1,60 +1,94 @@
 //variable declarations
 
-let choicesArray = ['rock', 'paper', 'scissors']
+let choicesArray = ["rock", "paper", "scissors"]
 let computerSelection
 let playerSelection
 let playerInput
 let scorestring
-let computerScore
-let playerScore
+let computerScore = 0
+document.getElementById("compScore").innerHTML = computerScore
+let playerScore = 0
+document.getElementById("plrScore").innerHTML = playerScore
 
 
+
+function updateDisplay(label,val) {
+    document.getElementById(label).innerHTML = val
+}
 
 function computerPlay() {
     random_int = Math.floor(Math.random()*choicesArray.length) 
     return choicesArray[random_int]
 }
 
-function displayScore(scorestring) {
-
-}
-
 function rockChoice() {
-    playerSelection = 'rock'
+    playerSelection = "rock"
     computerSelection = computerPlay()
+    playRound()
+    updateDisplay("compScore",computerScore)
+    updateDisplay("plrScore",playerScore)
+    
 }
 
 function scissorsChoice() {
-    playerSelection = 'scissors'
+    playerSelection = "scissors"
     computerSelection = computerPlay()
+    playRound()
+    updateDisplay("compScore",computerScore)
+    updateDisplay("plrScore",playerScore)
+
+
 }
 
 function paperChoice() {
-    playerSelection = 'paper'
+    playerSelection = "paper"
     computerSelection = computerPlay()
+    playRound()
+    updateDisplay("compScore",computerScore)
+    updateDisplay("plrScore",playerScore)
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
 
-    if(playerSelection == computerSelection){
+
+    if(playerSelection === computerSelection){
         //tie
+        alert("Tie!")
     }    
-    else if(playerSelection == 'rock' && computerSelection == 'paper'){
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
         //computer wins
+        alert("Computer wins!")
+        computerScore += 1
     }
-    else if(playerSelection == 'rock' && computerSelection == 'scissors'){
+    else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         //player wins
+        alert("Player wins")
+        playerScore += 1
     }
-    else if(playerSelection == 'paper' && computerSelection == 'rock'){
+    else if(playerSelection === 'paper' && computerSelection === 'rock'){
         //player wins
+        alert("Player wins")
+        playerScore += 1
+
+
     }
-    else if(playerSelection == 'paper' && computerSelection == 'scissors'){
+    else if(playerSelection === 'paper' && computerSelection === 'scissors'){
         //computer wins
+        alert("Computer wins!")
+        computerScore += 1
+
+
     }
-    else if(playerSelection == 'scissors' && computerSelection == 'paper'){
+    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         //player wins
+        alert("Player wins")
+        playerScore += 1
+
+
     }
-    else if(playerSelection == 'scissors' && computerSelection == 'rock'){
+    else if(playerSelection === 'scissors' && computerSelection === 'rock'){
         //computer wins
+        alert("Computer wins!")
+        computerScore += 1
     }
 }
